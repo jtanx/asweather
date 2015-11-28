3212e544086c56c87f3d5798aa30414f7667fd86
Jeremy Tan <jtanx@outlook.com>
2015-11-29 08:42:26 +1100

    ???

--- web/sydney.php
+++ web/sydney.php
@@ -8,10 +8,14 @@ function ftp_get_string($ftp, $filename) {
     if (@ftp_fget($ftp, $temp, $filename, FTP_BINARY, 0)) {
         rewind($temp);
         return stream_get_contents($temp);
+    } else {
+        return "Error reading data."
     }
 }
 
+
 $conn_id = ftp_connect("ftp2.bom.gov.au") or die("No connection to BOM");
+$login_result = ftp_login($conn_id, "anonymous", "");
 echo ftp_get_string($conn_id, "anon/gen/fwo/IDN10064.txt")
 ftp_close($conn_id);
 ?>
